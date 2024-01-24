7763604374d017e309e6a0f6811257469d769ea8
katigirl <katalin.tunde.kovacs@gmail.com>
2024-01-24 17:42:29 +0100

    Created a responsive grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
-      <h1>GameHub</h1>
+      <Grid gridTemplateAreas={{ base: `"navbar" "main" `, lg: `"navbar navbar" "sidebar main" ` }}>
+        <GridItem area={"navbar"} bg="papayawhip">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"sidebar"} bg="tomato">
+            Sidebar
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
