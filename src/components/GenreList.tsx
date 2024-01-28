b4b5e0115bab4b83bdcc9623c6be6b493ffdc672
katigirl <katalin.tunde.kovacs@gmail.com>
2024-01-28 17:32:55 +0100

    Changed the display to list and image

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,22 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error } = useGenres();
 
   return (
     <>
-      <ul>
+      <List>
         {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem key={genre.id} paddingY={1}>
+            <HStack>
+              <Image src={getCroppedImageUrl(genre.image_background)} boxSize="32px" borderRadius={8} />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
