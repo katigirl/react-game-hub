e7edbc489aee08618a50ddaf3bdd2dcda9caac12
katigirl <katalin.tunde.kovacs@gmail.com>
2024-01-31 11:46:01 +0100

    Add emojis showing rating

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -16,7 +17,9 @@ const GameCard = ({ game }: Props) => {
           <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={{ sm: "2xl", lg: "xl" }}>{game.name}</Heading>
+        <Heading fontSize={{ sm: "2xl", lg: "xl" }}>
+          {game.name} {<Emoji rating={game.rating_top} />}
+        </Heading>
       </CardBody>
     </Card>
   );
