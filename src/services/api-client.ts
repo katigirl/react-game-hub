75a3e70bbf2e10f9e87b135acf914f1412de25b5
katigirl <katalin.tunde.kovacs@gmail.com>
2024-01-31 17:01:40 +0100

    Changed to process env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
-  params: { key: "af02571c1fc84e2197e4a9977c3c172e" },
+  params: { key: process.env.VITE_API_KEY },
 });
