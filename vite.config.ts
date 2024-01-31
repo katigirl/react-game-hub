849876699f254704097e8c7c4cf451d26e50b13b
katigirl <katalin.tunde.kovacs@gmail.com>
2024-01-31 17:08:28 +0100

    Added prefix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "VITE_");
   return {
     define: {
       "process.env.VITE_API_KEY": JSON.stringify(env.VITE_API_KEY),
