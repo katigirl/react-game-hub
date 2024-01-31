883954640e3e1af6197547cafc88aade41526771
katigirl <katalin.tunde.kovacs@gmail.com>
2024-01-31 12:00:00 +0100

    Ship platforms with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
+import platforms from "../data/platforms";
 import useData from "./useData";
 import { Platform } from "./useGames";
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: platforms, error: null });
 
 export default usePlatforms;
