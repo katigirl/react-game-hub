465d20e19828c289757eb45d19eff0f704ae9882
katigirl <katalin.tunde.kovacs@gmail.com>
2024-02-06 16:57:19 +0100

    Refactoring- remove duplicate interfaces

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,7 +1,6 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import usePlatforms from "../hooks/usePlatforms";
-import { Platform } from "../hooks/useGames";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   selectedPlatform: Platform | null;
