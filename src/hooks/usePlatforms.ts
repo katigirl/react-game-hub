675875c8d163b0de7c1c166d38822ccd793a2940
katigirl <katalin.tunde.kovacs@gmail.com>
2024-02-06 16:10:43 +0100

    Change to react query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchRes } from "./useData";
+import { Platform } from "./useGames";
 
-const usePlatforms = () => ({ data: platforms, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () => apiClient.get<FetchRes<Platform>>("/platforms/lists/parents").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
