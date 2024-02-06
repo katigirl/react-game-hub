e3207b85b8af638286bc3a675cacc6c1eefd0d21
katigirl <katalin.tunde.kovacs@gmail.com>
2024-02-06 15:56:37 +0100

    Delete unused code

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -9,14 +9,12 @@ export interface Genre {
   slug: string;
 }
 
-const useGenres = () => {
-  return useQuery({
+const useGenres = () =>
+  useQuery({
     queryKey: ["genres"],
     queryFn: () => apiClient.get<FetchRes<Genre>>("/genres").then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000, //24h
     initialData: { count: genres.length, results: genres },
   });
-};
-// const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenres;
