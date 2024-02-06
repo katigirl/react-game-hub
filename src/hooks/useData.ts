e77659a6114a9ca9c4c0a7d1bfad6c3cdf403da7
katigirl <katalin.tunde.kovacs@gmail.com>
2024-02-06 15:54:06 +0100

    Adding export

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-interface FetchRes<T> {
+export interface FetchRes<T> {
   count: number;
   results: T[];
 }
