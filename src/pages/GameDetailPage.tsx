cea16ca0d7e9cebb2679f75dea5f58d3b480ac21
katigirl <katalin.tunde.kovacs@gmail.com>
2024-02-27 12:56:03 +0100

    Add GameScreenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -26,7 +27,7 @@ const GameDetailPage = () => {
       </GridItem>
       <GridItem>
         <GameTrailer gameId={game.id} />
-        {/* <GameScreenshots gameId={game.id} /> */}
+        <GameScreenshots gameId={game.id} />
       </GridItem>
     </SimpleGrid>
   );
